874b445df3c17d3b2474dc822053a351cb0f2a92
StevenMODIMO <stevepetes26@gmail.com>
2024-01-24 13:35:28 +0300

    mern-authentication

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center m-5">
+        <img
+          src="/jwt.png"
+          alt="logo"
+          className="w-24 h-24 rounded-full hover:shadow-lg hover:shadow-blue-950 hover:transition-all hover:duration-75 hover:ease-in-out hover:scale-105"
+        />
+      </div>
+      <p className="text-gray-400 text-center text-4xl p-5">
+        Mern Oauth (Node.JS, Express.JS, MongoDB, React) Passport.JS
+      </p>
+    </>
+  );
+}
+
+export default App;
